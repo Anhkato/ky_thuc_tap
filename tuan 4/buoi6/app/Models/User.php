f7df1f58e7aa7a5606f4c777bf8a293468b72d0d
anhkato <anhttph48908@gmail.com>
2025-05-27 14:19:16 +0700

    đẩy buôir 2 tuần 4

--- tuan 4/buoi6/app/Models/User.php
+++ tuan 4/buoi6/app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use Laravel\Fortify\TwoFactorAuthenticatable;
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable; use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function isAdmin()
+    {
+    return $this->role === 'admin';
+    }
+
 }
--- tuan 4/buoi6/resources/views/welcome.blade.php
+++ tuan 4/buoi6/resources/views/welcome.blade.php
@@ -1,1 +1,13 @@
-<img src="http://localhost/ass1/ass2.jpg" alt="" width="">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Đây Là Trang chủ</h1>
+
+
+</body>
+</html>
